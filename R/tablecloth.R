a51a41dcf03ab6395c4285564d760569eba8cdb9
Julianna Calabrese <juliannarosecalabrese@gmail.com>
2022-01-29 18:02:11 -0500

    Update tablecloth.R

--- R/tablecloth.R
+++ R/tablecloth.R
@@ -2,9 +2,6 @@
 #'
 #' @name tablecloth
 #' @description This is where I'm putting functions that are helping me finish my Master's.
-#' @details
-#' \if{html}{\figure{sticker.PNG}{options: width="20%"}}
-#' @md
 #' @author Julianna Calabrese
 #' @docType package
 #'
