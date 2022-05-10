4d9f597ce05d2fbdf75912bacbcf7059a81c67b2
‘Julianna <juliannarosecalabrese@gmail.com>
2022-05-10 11:51:37 -0400

    updated clear_out()
    added more arguments, can allow some plot background with the rest being transparent

--- R/clear_out.R
+++ R/clear_out.R
@@ -1,14 +1,16 @@
 #' Apply clear background to ggplot.
 #'
 #' Applies a totally-clear/transparent background to a ggplot2 object as a theme.
+#' Uses `theme_bw()`.
 #'
 #' @param clr Desired outline color.
+#' @param bg Should background be transparent? Binary.
 #'
 #' @importFrom ggplot2 theme_bw theme
 #' @export
-clear_out <- function(clr){
+clear_out <- function(clr, bg) {
 
-  element_blank <- element_rect <- element_text <- element_line <- NULL
+  element_blank <- element_rect <- element_text <- element_line <- rel <- NULL
 
   # If color is not specified, default to black
   if (missing(clr))
@@ -16,11 +18,30 @@ clear_out <- function(clr){
   else
     do_nothing <- "nothing"
 
+  # If bg is not specified, default to TRUE
+  if (missing(bg))
+    bg <- TRUE
+  else
+    do_nothing_again <- "nothing"
+
+  if (bg == TRUE) {
+    backg <- "transparent"
+    backline <- "transparent"
+    pg <- element_blank()
+    pmn <- element_blank()
+    }
+  else {
+    backg <- "white"
+    backline <- "grey85"
+    pg <- element_line(colour = "grey85")
+    pmn <- element_line(size = rel(0.5))
+    }
+
   ggplot2::theme_bw() + ggplot2::theme(
-    legend.position = "right",
+    panel.grid = pg,
     panel.grid.minor = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.background = element_rect(fill = "transparent"),
+    panel.grid.major = pmn,
+    panel.background = element_rect(fill = backg),
     plot.background = element_rect(fill = "transparent", color = NA),
     legend.text = element_text(color = clr),
     legend.background = element_blank(),
@@ -32,7 +53,7 @@ clear_out <- function(clr){
     axis.ticks = element_line(color = clr),
     text = element_text(color = clr),
     axis.text = element_text(color = clr),
-    strip.background = element_rect(color = clr, fill = "transparent"),
-    strip.text = element_text(color = clr))
+    strip.background = element_rect(color = clr, fill = backline),
+    strip.text = element_text(color = clr), complete = TRUE)
 }
 
